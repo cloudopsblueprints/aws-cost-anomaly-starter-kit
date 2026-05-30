5fd4d94c83c669a9c5569cce3551761d91d36736
lbroom <lbroom@aicpa-cima.com>
2026-05-30 14:55:00 +0100

    Add initial AWS Cost Anomaly Detection Terraform implementation

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,9 @@
+output "sns_topic_arn" {
+  description = "SNS topic used for cost anomaly notifications"
+  value       = aws_sns_topic.cost_alerts.arn
+}
 
+output "cost_monitor_arn" {
+  description = "Cost anomaly monitor ARN"
+  value       = aws_ce_anomaly_monitor.service_monitor.arn
+}
